ae88d20de223c3758c32c3da2b8ec56db6b025a6
IngDannyGianca <87837006+IngDannyGianca@users.noreply.github.com>
2021-09-11 14:28:29 -0600

    Add files via upload

---
+++ Grupo 5/Danny Saldaña Usuarios/CapaControlador/clsControlador.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaModelo;
+using System.Data;
+using System.Data.Odbc;
+
+namespace CapaControlador
+{
+    public class clsControlador
+    {
+        clsSentencias sn = new clsSentencias();
+        // LLenar una tabla CAPA CONTROLADOR
+
+
+        public DataTable llenarTbl(string tabla)
+        {
+            OdbcDataAdapter dt = sn.llenarTbl(tabla);
+            DataTable table = new DataTable();
+            dt.Fill(table);
+            return table;
+        }
+    }
+}
