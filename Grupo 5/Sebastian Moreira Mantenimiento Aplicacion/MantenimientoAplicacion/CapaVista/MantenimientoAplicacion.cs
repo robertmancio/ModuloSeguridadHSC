9ba1f581cfcf991e6ecf48ebc3fb5e28686a31b9
kievkevin13 <kievkevin13@gmail.com>
2021-09-18 13:43:23 -0600

    Mantenimiento Aplicacion HSC

---
+++ Grupo 5/Sebastian Moreira Mantenimiento Aplicacion/MantenimientoAplicacion/CapaVista/MantenimientoAplicacion.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaControlador;
+
+namespace CapaVista
+{
+    public partial class MantenimientoAplicacion : UserControl
+    {
+        clsControladorAplicacion conAplicacion = new clsControladorAplicacion();
+        public MantenimientoAplicacion()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrueba_Click(object sender, EventArgs e)
+        {
+            //funInsertar();
+            //funBuscar();
+            //funLimpiar();
+            //funEliminar();
+            //funModificar();
+
+            
+            
+            try
+            {
+                if (btnHabilitado.Checked == true)
+                {
+                    conAplicacion.insertarAplicacion(txtIdAplicacion.Text, txtNombreAplicacion.Text, 1);
+                    MessageBox.Show("Inserción realizada");
+                    txtIdAplicacion.Text = "";
+                    txtNombreAplicacion.Text = "";
+                    btnInhabilitado.Checked = false;
+                    btnHabilitado.Checked = false;
+                }
+                else if (btnInhabilitado.Checked == true)
+                {
+                    conAplicacion.insertarAplicacion(txtIdAplicacion.Text, txtNombreAplicacion.Text, 0);
+                    MessageBox.Show("Inserción realizada");
+                    txtIdAplicacion.Text = "";
+                    txtNombreAplicacion.Text = "";
+                    btnInhabilitado.Checked = false;
+                    btnHabilitado.Checked = false;
+                }
+                else
+                {
+                    MessageBox.Show("No ha seleccionado estado!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex);
+            }
+
+
+
+        }
+        public void funLimpiar()
+        {
+            txtIdAplicacion.Text = "";
+            txtNombreAplicacion.Text = "";
+            btnHabilitado.Checked = false;
+            btnInhabilitado.Checked = false;
+        }
+
+    }
+}
